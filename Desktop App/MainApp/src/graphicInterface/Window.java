7e0be6405182e9947b33c633f8f20c630484f7a4
urrutiaitor <aitorurrutiazubikarai@gmail.com>
2015-11-25 09:04:20 +0100

    Stable base of the graphic interface
    The menu is ok.
    Lenguaje change is ok.
    Reading data from files is ok.
    
    PENDING: Fill tabs with data.

--- Desktop App/MainApp/src/graphicInterface/Window.java
+++ Desktop App/MainApp/src/graphicInterface/Window.java
@@ -26,7 +26,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
-public class Window extends JFrame implements ActionListener, Observer{
+public class Window extends JFrame implements ActionListener, Observer {
 
 	/**
 	 * 
@@ -35,22 +35,23 @@ public class Window extends JFrame implements ActionListener, Observer{
 
 	String tabName[][];
 	String menuName[][];
-	
+
 	int selectedLenguage = 2;
-	
+
 	String tabFilePath = "tabFile.txt";
 	String menuFilePath = "menuFile.txt";
-	
+
 	final static int LENGUAGECANT = 3;
 	final static int TABCANT = 3;
-	final static int MENUCANT = 8;
-	
-	AbstractAction accOptionMenu, accOptionEdit;
-	AbstractAction accLenguage1, accLenguage2, accLenguage3;
-	
-	public Window () {
+	final static int MENUCANT = 9;
+
+	JTabbedPane tabbedPane;
+	JMenu menuFile, menuEdit, menuConfiguration, lenguage;
+	JMenuItem optionMenu, optionEdit;
+	JMenuItem lenguage1, lenguage2, lenguage3;
+
+	public Window() {
 		initializeNames();
-		createActions();
 		this.setJMenuBar(createMenuBar());
 		this.setContentPane(createFrame());
 		this.setLocation(200, 200);
@@ -59,32 +60,23 @@ public class Window extends JFrame implements ActionListener, Observer{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	public void initializeNames () {
+	public void initializeNames() {
 		tabName = new String[TABCANT][LENGUAGECANT];
 		menuName = new String[MENUCANT][LENGUAGECANT];
-		
+
 		for (int i = 0; i < TABCANT; i++) {
 			tabName[i] = getString(tabFilePath, "TAB" + String.valueOf(i));
 		}
-		
+
 		for (int i = 0; i < MENUCANT; i++) {
 			menuName[i] = getString(menuFilePath, "MENU" + String.valueOf(i));
 		}
 	}
-	
-	private void createActions() {
-		accOptionMenu = new MyAction (this, menuName[1][selectedLenguage], new ImageIcon("icons/optionMenuIcon.png"), KeyEvent.VK_C);
-		accOptionEdit = new MyAction (this, menuName[3][selectedLenguage], new ImageIcon("icons/optionEditIcon.png"), KeyEvent.VK_C);
-		accLenguage1 = new MyAction (this, menuName[5][selectedLenguage], new ImageIcon("icons/lenguage1Icon.png"), KeyEvent.VK_C);
-		accLenguage2 = new MyAction (this, menuName[6][selectedLenguage], new ImageIcon("icons/lenguage2Icon.png"), KeyEvent.VK_C);
-		accLenguage3 = new MyAction (this, menuName[7][selectedLenguage], new ImageIcon("icons/lenguage3Icon.png"), KeyEvent.VK_C);
-		
-	}
-	
-	public String[] getString (String pathname, String name) {
+
+	public String[] getString(String pathname, String name) {
 		String line = null;
 		String data[];
-		
+
 		File file = new File(pathname);
 		FileReader fr;
 		try {
@@ -94,7 +86,7 @@ public class Window extends JFrame implements ActionListener, Observer{
 			return null;
 		}
 		BufferedReader br = new BufferedReader(fr);
-		
+
 		try {
 			while ((line = br.readLine()) != null) {
 				data = line.split("%");
@@ -113,81 +105,105 @@ public class Window extends JFrame implements ActionListener, Observer{
 		} catch (IOException e) {
 			System.err.println("Error trying to read line from file " + pathname + " to find " + name);
 		}
-		
+
 		System.err.println("Error trying to find type " + name + ": NOT FOUND");
 		return null;
-		
+
 	}
 
 	private Container createFrame() {
-		JTabbedPane tabbedPane = new JTabbedPane();
-		
+		tabbedPane = new JTabbedPane();
+
 		tabbedPane.addTab(tabName[0][selectedLenguage], new ImageIcon("icons/tabIcon1.png"), new Tab1());
 		tabbedPane.addTab(tabName[1][selectedLenguage], new ImageIcon("icons/tabIcon2.png"), new Tab2());
 		tabbedPane.addTab(tabName[2][selectedLenguage], new ImageIcon("icons/tabIcon3.png"), new Tab3());
-		
+
 		return tabbedPane;
 	}
 
 	private JMenuBar createMenuBar() {
 		JMenuBar bar = new JMenuBar();
-		
+
 		bar.add(createMenuFile());
 		bar.add(createMenuEdit());
 		bar.add(Box.createHorizontalGlue());
 		bar.add(createMenuConfiguration());
-		
+
 		return bar;
 	}
 
 	private JMenu createMenuFile() {
-		JMenu menuFile = new JMenu (menuName[0][selectedLenguage]);
+		menuFile = new JMenu(menuName[0][selectedLenguage]);
 		menuFile.add(createMenuFileInside());
 		return menuFile;
 	}
 
 	private JMenuItem createMenuFileInside() {
-		JMenuItem optionMenu = new JMenuItem(accOptionMenu);
-		
+		optionMenu = new JMenuItem(menuName[1][selectedLenguage]);
+		optionMenu.addActionListener(this);
 		return optionMenu;
 	}
 
 	private JMenu createMenuEdit() {
-		JMenu menuEdit = new JMenu (menuName[2][selectedLenguage]);
+		menuEdit = new JMenu(menuName[2][selectedLenguage]);
 		menuEdit.add(createMenuEditInside());
 		return menuEdit;
 	}
 
 	private JMenuItem createMenuEditInside() {
-		JMenuItem optionEdit = new JMenuItem(accOptionEdit);
-		
+		optionEdit = new JMenuItem(menuName[3][selectedLenguage]);
+		optionEdit.addActionListener(this);
 		return optionEdit;
 	}
 
 	private JMenu createMenuConfiguration() {
-		JMenu menuConfiguration = new JMenu (menuName[4][selectedLenguage]);
+		menuConfiguration = new JMenu(menuName[4][selectedLenguage]);
 		menuConfiguration.add(createMenuConfigurationLenguage());
 		return menuConfiguration;
 	}
 
 	private JMenuItem createMenuConfigurationLenguage() {
-		JMenu lenguage = new JMenu(menuName[5][selectedLenguage]);
-		lenguage.add(accLenguage1);
-		lenguage.add(accLenguage2);
-		lenguage.add(accLenguage3);
+		lenguage = new JMenu(menuName[5][selectedLenguage]);
+
+		lenguage.add(lenguage1 = new JMenuItem(menuName[6][selectedLenguage]));
+		lenguage.add(lenguage2 = new JMenuItem(menuName[7][selectedLenguage]));
+		lenguage.add(lenguage3 = new JMenuItem(menuName[8][selectedLenguage]));
+
+		lenguage1.addActionListener(this);
+		lenguage2.addActionListener(this);
+		lenguage3.addActionListener(this);
+
 		return lenguage;
-		
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		String ac = e.getActionCommand();
+
+		if (menuName[1][selectedLenguage].matches(ac)) {
+			System.out.println("SubFile");
+		}
+		if (menuName[3][selectedLenguage].matches(ac)) {
+			System.out.println("SubEdit");
+		}
+		if (menuName[6][selectedLenguage].matches(ac)) {
+			selectedLenguage = 0;
+			refresh();
+		}
+		if (menuName[7][selectedLenguage].matches(ac)) {
+			selectedLenguage = 1;
+			refresh();
+		}
+		if (menuName[8][selectedLenguage].matches(ac)) {
+			selectedLenguage = 2;
+			refresh();
+		}
 	}
 
 	public String[][] getTabName() {
@@ -203,7 +219,27 @@ public class Window extends JFrame implements ActionListener, Observer{
 	}
 
 	public void setSelectedLenguage(int selectedLenguage) {
+		System.out.println("Selected lenguage: " + selectedLenguage);
 		this.selectedLenguage = selectedLenguage;
+	}
+
+	public void refresh() {
+		tabbedPane.setTitleAt(0, tabName[0][selectedLenguage]);
+		tabbedPane.setTitleAt(1, tabName[1][selectedLenguage]);
+		tabbedPane.setTitleAt(2, tabName[2][selectedLenguage]);
+
+		menuFile.setText(menuName[0][selectedLenguage]);
+		optionMenu.setText(menuName[1][selectedLenguage]);
+		
+		menuEdit.setText(menuName[2][selectedLenguage]);
+		optionEdit.setText(menuName[3][selectedLenguage]);
+		
+		menuConfiguration.setText(menuName[4][selectedLenguage]);
+		lenguage.setText(menuName[5][selectedLenguage]);
+		lenguage1.setText(menuName[6][selectedLenguage]);
+		lenguage2.setText(menuName[7][selectedLenguage]);
+		lenguage3.setText(menuName[8][selectedLenguage]);
+
 	}
 
 }
