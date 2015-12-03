c8adc83c032b87b6da0c21d994dcf0c1860fa5df
urrutiaitor <aitorurrutiazubikarai@gmail.com>
2015-12-03 18:34:52 +0100

    Reader & writer
    Reader and writer classes improved

--- Desktop App/MainApp/src/operation/Reader.java
+++ Desktop App/MainApp/src/operation/Reader.java
@@ -13,11 +13,16 @@ public class Reader {
 		String data[];
 
 		File file = new File(pathname);
-		FileReader fr;
+		FileReader fr = null;
 		try {
 			fr = new FileReader(file);
 		} catch (FileNotFoundException e1) {
 			System.err.println("Error trying to start reading file " + pathname + " to find " + name);
+			try {
+				fr.close();
+			} catch (IOException e) {
+				System.err.println("Error trying to close file " + pathname + " to find" + name);
+			}
 			return null;
 		}
 		
@@ -29,6 +34,8 @@ public class Reader {
 				data = line.split("%");
 				if (data[0] == null) {
 					System.err.println("Erros trying to split line in file " + pathname + " to find " + name);
+					br.close();
+					fr.close();
 					return null;
 				}
 				if (data[0].matches(name)) {
@@ -36,6 +43,8 @@ public class Reader {
 					for (int i = 0; i < data.length - 1; i++) {
 						returnData[i] = data[i + 1];
 					}
+					br.close();
+					fr.close();
 					return returnData;
 				}
 			}
@@ -48,4 +57,70 @@ public class Reader {
 
 	}
 	
+	public static int getLastId (String pathname) {
+		String line = null;
+		String lineAux = null;
+		String data[];
+
+		File file = new File(pathname);
+		FileReader fr = null;
+		try {
+			fr = new FileReader(file);
+		} catch (FileNotFoundException e1) {
+			System.err.println("Error trying to start reading file " + pathname);
+			try {
+				fr.close();
+			} catch (IOException e) {
+				System.err.println("Error trying to close file " + pathname);
+			}
+			return -1;
+		}
+		
+		BufferedReader br = new BufferedReader(fr);
+
+		try {
+			while ((lineAux = br.readLine()) != null) {
+				line = lineAux;
+			}
+			data = line.split("%");
+			if (data[0] == null) {
+				System.err.println("Erros trying to split line in file " + pathname);
+				fr.close();
+				br.close();
+				return -1;
+			}
+			fr.close();
+			br.close();
+			return getNum(data[0]);
+		} catch (IOException e) {
+			System.err.println("Error trying to read line from file");
+		}
+		
+		try {
+			fr.close();
+			br.close();
+		} catch (IOException e) {
+			System.err.println("Error trying to close file");
+		}
+		return -1;
+	}
+
+	private static int getNum(String string) {
+		int firstIndex = 0;
+		String result = "";
+		
+		for (int i = 0; i < string.length(); i++) {
+			if (string.charAt(i) > '0' && string.charAt(i) < '9') {
+				firstIndex = i;
+				break;
+			}
+		}
+		
+		for (int i = firstIndex; i < string.length(); i++) {
+			result = result.concat(String.valueOf(string.charAt(i)));
+		}
+		
+		return Integer.parseInt(result);
+	}
+
 }
--- Desktop App/MainApp/src/operation/Writter.java
+++ Desktop App/MainApp/src/operation/Writter.java
@@ -1,5 +1,51 @@
 package operation;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Writter {
 
+	public static boolean setAction(String pathname, int action) {
+		
+		File file = new File(pathname);
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(file);
+		} catch (IOException e2) {
+			System.err.println("Error openin File Writer to write action " + action + " in file " + pathname);
+			try {
+				fw.close();
+			} catch (IOException e) {
+				System.err.println("Error trying to close File Writer after error");
+			}
+		}
+		BufferedWriter bw = new BufferedWriter(fw);
+
+		int position = Reader.getLastId(pathname);
+		
+		String str = "ACTION" + position + "%" + System.currentTimeMillis() + "%" + action;
+		
+		try {
+			bw.write(str);
+			bw.close();
+			fw.close();
+			
+			return true;
+		} catch (IOException e) {
+			System.err.println("Error trying to write action " + action + " in file " + pathname);
+			try {
+				bw.close();
+				fw.close();
+			} catch (IOException e1) {
+				System.err.println("Error trying to close file after a writting error");
+				return false;
+			}
+			
+			return false;
+		}
+
+	}
+	
 }
