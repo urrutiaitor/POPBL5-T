8854e944fd79b562368df34246cb61caf4af977b
urrutiaitor <aitorurrutiazubikarai@gmail.com>
2016-01-19 10:37:17 +0100

    Rest
    Add remaining class

---
+++ Desktop App/UserApp/src/connection/SocketConnection.java
@@ -0,0 +1,101 @@
+package connection;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.concurrent.Semaphore;
+
+import operation.House;
+
+public class SocketConnection {
+
+	House house;
+	int user;
+	long initTime;
+	
+	Socket clientSocket = null;
+	protected int serverPort = 8080;
+	String hostName = "localhost";
+	
+	Semaphore mutex;
+	
+	InputStream input;
+	OutputStream output;
+	
+	DataInputStream dataInput;
+	DataOutputStream dataOutput;
+	
+	public SocketConnection(House house){
+		this.house = house;
+		
+		try {
+			clientSocket = new Socket(hostName, serverPort);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		mutex = new Semaphore(1);
+		
+		try {
+			input = clientSocket.getInputStream();
+			output = clientSocket.getOutputStream();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		dataInput = new DataInputStream(input);
+		dataOutput = new DataOutputStream(output);
+	}
+	
+	public String tryRead(){
+		String line = null;
+		
+		try {
+			mutex.acquire();
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			line = dataInput.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			mutex.release();
+			return null;
+		}
+		
+		mutex.release();
+		
+		return line;
+	}
+	
+	public boolean tryWrite(String str){
+		try {
+			mutex.acquire();
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			dataOutput.writeBytes(str);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			mutex.release();
+			return false;
+		}
+		
+		mutex.release();
+		
+		return true;
+	}
+	
+}
